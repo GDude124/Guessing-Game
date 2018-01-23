daf7e7e2123f2fc2ee9723ac3dc8df8e93db450d
Elijah Estes <elijahestes@Interapts-MacBook-Pro-4.local>
2018-01-23 15:44:41 -0500

    first commit

---
+++ The Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Guessing Game
+//
+//  Created by Elijah Estes on 1/23/18.
+//  Copyright © 2018 Elijah Estes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
